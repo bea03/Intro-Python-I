d2ca3e629385eeff55b4b47e061966ec0d00c843
Nicole Beamer <tulip@Nicoles-iMac.hsd1.ut.comcast.net>
2020-06-02 14:55:47 -0600

    11 done

--- src/11_args.py
+++ src/11_args.py
@@ -5,6 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(intOne, intTwo):
+    return intOne + intTwo
 
 print(f1(1, 2))
 
@@ -13,6 +15,11 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(*ints):
+    sum = 0
+    for x in ints:
+        sum += x
+    return sum
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -22,7 +29,7 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22 ++The * operator tells the function to unpack the list first
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -30,7 +37,10 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+'''if your give the second value a default value of one, that will be the value to add one to it.Otherwise, the value pair value will override the default value. The default is only used if there is no second given value'''
 
+def f3(num, num2 = 1):
+    return num + num2
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
@@ -44,6 +54,10 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+'''use an ** operator before the parameter name to denote an arbitrary keyword arg'''
+def f4(**pairs):
+    for x,y in pairs.items():
+        print(f"key: {x}, value: {y}")
 
 # Should print
 # key: a, value: 12
@@ -62,4 +76,4 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d)
